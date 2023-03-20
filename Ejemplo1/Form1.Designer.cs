e6bff508f8d6ac73ef63e5dbaa757220680e6711
william <ofeliavm69@gmail.com>
2023-03-20 17:50:20 -0400

    Primer programa suma

---
+++ Ejemplo1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Ejemplo1
+{
+    partial class Aplicacion
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_entrada1 = new System.Windows.Forms.TextBox();
+            this.txt_entrada2 = new System.Windows.Forms.TextBox();
+            this.btb_suma = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lbl_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titulo.Location = new System.Drawing.Point(265, 37);
+            this.lbl_titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(345, 39);
+            this.lbl_titulo.TabIndex = 0;
+            this.lbl_titulo.Text = "Suma de 2 Numeros";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(269, 123);
+            this.lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(139, 32);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Numero 1:";
+            this.lbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(269, 161);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero 2:";
+            // 
+            // txt_entrada1
+            // 
+            this.txt_entrada1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_entrada1.Location = new System.Drawing.Point(463, 116);
+            this.txt_entrada1.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_entrada1.Name = "txt_entrada1";
+            this.txt_entrada1.Size = new System.Drawing.Size(132, 39);
+            this.txt_entrada1.TabIndex = 3;
+            // 
+            // txt_entrada2
+            // 
+            this.txt_entrada2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_entrada2.Location = new System.Drawing.Point(463, 161);
+            this.txt_entrada2.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_entrada2.Name = "txt_entrada2";
+            this.txt_entrada2.Size = new System.Drawing.Size(132, 39);
+            this.txt_entrada2.TabIndex = 4;
+            this.txt_entrada2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btb_suma
+            // 
+            this.btb_suma.BackColor = System.Drawing.Color.Teal;
+            this.btb_suma.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btb_suma.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btb_suma.ForeColor = System.Drawing.Color.White;
+            this.btb_suma.Location = new System.Drawing.Point(335, 334);
+            this.btb_suma.Margin = new System.Windows.Forms.Padding(4);
+            this.btb_suma.Name = "btb_suma";
+            this.btb_suma.Size = new System.Drawing.Size(215, 51);
+            this.btb_suma.TabIndex = 5;
+            this.btb_suma.Text = "SUMAR";
+            this.btb_suma.UseVisualStyleBackColor = false;
+            this.btb_suma.Click += new System.EventHandler(this.btb_suma_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(272, 262);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "El resultado es:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_resultado.Location = new System.Drawing.Point(496, 262);
+            this.lbl_resultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(54, 31);
+            this.lbl_resultado.TabIndex = 7;
+            this.lbl_resultado.Text = "----";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(424, 136);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(390, 222);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(254, 31);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "_______________";
+            // 
+            // Aplicacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(902, 527);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btb_suma);
+            this.Controls.Add(this.txt_entrada2);
+            this.Controls.Add(this.txt_entrada1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.lbl_titulo);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Aplicacion";
+            this.Text = "Aplicacion de Suma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_entrada1;
+        private System.Windows.Forms.TextBox txt_entrada2;
+        private System.Windows.Forms.Button btb_suma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Ejemplo1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo1
+{
+    public partial class Aplicacion : Form
+    {
+        public Aplicacion()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btb_suma_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt32(txt_entrada1.Text);
+            int x2 = Convert.ToInt32(txt_entrada2.Text);
+            int suma = x1 + x2;
+            lbl_resultado.Text = suma.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
